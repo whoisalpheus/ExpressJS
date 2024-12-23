8d81f2fb892a90fb0107c4ac1935811b29184953
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2024-12-23 13:25:25 -0500

    Continuing ExpressJs docs...

--- myapp/app.js
+++ myapp/app.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+// Static files direction...
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+// Routes
+
 app.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+app.post('/', (req, res) => {
+    res.send('Got a POST request')
+})
+
+app.put('/user', (req, res) => {
+    res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', (req, res) => {
+    res.send('Got a DELETE request at /user')
 })
 
 app.listen(port, () => {
